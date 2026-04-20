a5e54e950626d569f0e909911b76e0b0564b9d9e
Osretna <127016877+Osretna@users.noreply.github.com>
2026-04-20 09:22:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,12 @@
-const cacheName = 'assoc-v1';
-const assets = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/logo.png',
-  '/manifest.json'
-];
+const cacheName = 'jamaiya-v1';
+const assets = ['./', './index.html', './style.css', './app.js', './logo.png'];
 
-// تثبيت ملفات الكاش
+// تثبيت التطبيق وتخزين الملفات للعمل بدون انترنت
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      cache.addAll(assets);
-    })
-  );
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-// استدعاء الملفات حتى عند انقطاع الانترنت
+// تشغيل التطبيق من الكاش
 self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(res => {
-            return res || fetch(e.request);
-        })
-    );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
